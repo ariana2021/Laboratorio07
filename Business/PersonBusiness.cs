45ad244c575e83e328fb3125854e3258dfaf4ab5
Alumno <Alumno@LAB1504-03>
2024-04-27 16:41:15 -0500

    Agregar archivos de proyecto.

---
+++ Business/PersonBusiness.cs
@@ -0,0 +1,28 @@
+﻿using Data;
+using Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business
+{
+    public class PersonBusiness
+    {
+        public List<Person> Get()
+        {
+            PersonData data = new PersonData();
+            var people = data.Get();
+            return people;
+        }
+
+        public List<Person> Get18() 
+        {
+            PersonData data = new PersonData();
+            var people = data.Get();
+            var result = people.Where(x => x.Age > 18).ToList();
+            return result;
+        }
+    }
+}
---
+++ Data/PersonData.cs
@@ -0,0 +1,55 @@
+﻿using Entity;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    public class PersonData
+    {
+        string connectionString = "Data Source=LAB1504-03\\SQLEXPRESS;Initial Catalog=FacturaDB;User Id=hugo;Password=123456";
+        public List<Person> Get()
+        {
+            List<Person> people = new List<Person>();
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                using (SqlCommand command = new SqlCommand("USP_ListProduct", connection))
+                {
+                    command.CommandType = CommandType.StoredProcedure;
+
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            Person person = new Person();
+                            person.PersonID = Convert.ToInt32(reader["PersonID"]);
+                            person.FirstName = reader["FirstName"].ToString();
+                            person.LastName = reader["LastName"].ToString();
+                            person.Age = Convert.ToInt32(reader["Age"]);
+                            people.Add(person);
+                        }
+                        reader.Close();
+                    }
+                    return people;
+                }
+            }
+        }
+    
+        public void Insert()
+        {
+        }
+        public void Update()
+        {
+        }
+        public void Delete()
+        {
+        }
+    }
+}
